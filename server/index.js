10186817b27866bfa683520b8b236f3c3e565a54
animesh <animeshrathore255@gmail.com>
2025-02-13 17:19:49 +0530

    feat: added index.js initial server setup

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv').config()
+const port = process.env.PORT
+var cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req,res) => {
+    res.json('Recyclink project')
+})
+
+app.listen(port, () => {
+    console.log(`Server started at ${port}`)
+})
+
