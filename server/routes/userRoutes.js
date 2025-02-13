378189b2fac966eeb3e4bb56d9797d70aae1cb83
animesh <animeshrathore255@gmail.com>
2025-02-13 17:28:30 +0530

    feat: added userRoutes.js file

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+router.post('/register', () => {
+    // register route controller will be here
+})
+
+router.post('/sign', () => {
+    // login route logic controller will be here
+})
+
+module.exports = router;
