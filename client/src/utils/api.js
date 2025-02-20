0f316da5cd47704984378fba8d54d1ccfd8dc81d
animesh <animeshrathore255@gmail.com>
2025-02-20 23:40:04 +0530

    fixed backend cookies bug

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:6478/api", // for production
+  
+  baseURL: "https://recyc-link-backend.vercel.app/api", // for production
+
+  // baseURL: "http://localhost:6478/api", // for development
 
   withCredentials: true,
 });
